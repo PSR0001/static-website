b260c6bd978a8d91b8d808db8b9bd3aed627dd32
Partha Singha Roy <parthasingharoy166@gmail.com>
2022-01-31 16:27:17 +0530

    add pug

--- app.js
+++ app.js
@@ -8,9 +8,12 @@ const server = http.createServer(app);
 const port = process.env.PORT || 3000
 
 // app.use(express.static('public'))
-app.use('/', express.static('public'))
+// app.use('/', express.static('public'))
+app.set('view engine', 'pug')
 
-
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!' })
+})
 
 server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
